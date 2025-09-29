055a0187831ec92864afd32efb357993a6ad1be9
Amanda Thompson <thompson.adg@gmail.com>
2025-09-29 11:41:41 -0700

    wave_01 updated

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,17 @@
 class Vendor:
-    pass
+# Vendor class constructor with empty arrary initialization
+    def __init__(self, inventory=None):
+        inventory = [] if inventory is None else inventory
+        self.inventory = inventory
+
+# Instance method to add to the inventory
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+    
+    def remove(self, item):
+        if item not in self.inventory:
+            return False
+        else:
+            self.inventory.remove(item)
+            return item 
