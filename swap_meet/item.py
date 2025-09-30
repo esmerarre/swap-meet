247a12b2fe0d6987e516204f65dfed3864f2909c
Amanda Thompson <thompson.adg@gmail.com>
2025-09-29 23:00:04 -0700

    implemented wave_02-06 with test assertions

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,28 @@
+import uuid
+
 class Item:
-    pass
+    def __init__(self, id=None, condition=0):
+        self.id = uuid.uuid4().int if id is None else id
+        self.condition = condition
+        
+    def get_category(self):
+        return "Item"
+    
+    def __str__(self):
+        return f"An object of type Item with id {self.id}."
+    
+    def condition_description(self):
+        if self.condition == 0:
+            return "Condition 0"
+        elif self.condition == 1:
+            return "Condition 1"
+        elif self.condition == 2:
+            return "Condition 2"
+        elif self.condition == 3:
+            return "Condition 3"
+        elif self.condition == 4:
+            return "Condition 4"
+        elif self.condition == 5:
+            return "Condition 5"
+        
+        raise ValueError("Unknown Condition Type")
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,18 +1,82 @@
+from .item import Item
+
 class Vendor:
-# Vendor class constructor with empty arrary initialization
     def __init__(self, inventory=None):
         inventory = [] if inventory is None else inventory
         self.inventory = inventory
 
-# Instance method to add to the inventory
     def add(self, item):
         self.inventory.append(item)
         return item
 
-# Instance method to remove item in inventory
     def remove(self, item):
         if item not in self.inventory:
             return False
         else:
             self.inventory.remove(item)
             return item 
+
+    def get_by_id(self, id):
+        for item in self.inventory:
+            if item.id == id:
+                return item 
+        return None
+    
+    def swap_items(self, other_vendor, my_item, their_item):
+        my_item_index = -1
+        their_item_index = -1
+
+        for index in range(len(self.inventory)):
+            item = self.inventory[index]
+            if my_item == item:
+                my_item_index = index 
+
+        for index in range(len(other_vendor.inventory)):
+            item = other_vendor.inventory[index]
+            if their_item == item:
+                their_item_index = index
+
+        if my_item_index > -1 and their_item_index > -1:
+            other_vendor.inventory.append(self.inventory.pop(my_item_index))
+            self.inventory.append(other_vendor.inventory.pop(their_item_index))
+            return True
+
+        return False
+    
+    def swap_first_item(self, other_vendor):
+        if len(self.inventory) == 0 or len(other_vendor.inventory) == 0:
+            return False
+
+        temp_item = self.inventory[0]
+        self.inventory[0] = other_vendor.inventory[0]
+        other_vendor.inventory[0] = temp_item
+        return True
+    
+    def get_by_category(self, category):
+        items_with_category = []
+
+        for item in self.inventory:
+            if item.get_category() == category:
+                items_with_category.append(item)
+
+        return items_with_category
+
+    def get_best_by_category(self, category):
+        items_with_category = self.get_by_category(category)
+        best_condition = -1
+        item_to_return = None
+        for item in items_with_category:
+            if item.condition > best_condition:
+                best_condition = item.condition
+                item_to_return = item
+
+        return item_to_return
+    
+    def swap_best_by_category(self, other_vendor, my_priority, their_priority):
+        my_best_category = self.get_best_by_category(my_priority)
+        their_best_category = other_vendor.get_best_by_category(their_priority)
+
+        if my_best_category is None or their_best_category is None:
+            return False
+        
+        self.swap_items(other_vendor, my_best_category, their_best_category)
